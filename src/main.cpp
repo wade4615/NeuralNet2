f19c8c0fa23f2615e9cf8a4f758d54ae016155b9
WADEOS <wade4615@gmail.com>
2020-09-23 15:52:37 -0500

    comverted the 2 weights variables to one array

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,6 @@ using namespace std;
 
 int numberOfLayers;
 
-Matrix inputMiddleWeights;
-Matrix middleOutputWeights;
-
 Matrix deltaWeightInputMiddle;
 Matrix deltaWeightMiddleOutput;
 Array outputLayerDelta;
@@ -50,6 +47,7 @@ NetworkType alpha;
 NetworkType eta;
 
 MatrixPtr layer;
+MatrixPtr weights;
 
 void initialize(int configuration[], int example, int bias) {
     trainingInput = NULL;
@@ -73,12 +71,10 @@ void initialize(int configuration[], int example, int bias) {
     allocateMatrix(&layer[0], trainBias, inputBias, 0.0);
     allocateMatrix(&layer[1], trainBias, middleBias, 0.0);
     allocateMatrix(&layer[2], trainBias, outputBias, 0.0);
-    //inputLayer = &layer[0];
-    //middleLayer = &layer[1];
-    //outputLayer = &layer[2];
 
-    allocateMatrix(&inputMiddleWeights, inputBias, middleBias, fRand(-1.0, 1.0));
-    allocateMatrix(&middleOutputWeights, middleBias, outputBias, fRand(-1.0, 1.0));
+    weights = new Matrix[2];
+    allocateMatrix(&weights[0], inputBias, middleBias, fRand(-1.0, 1.0));
+    allocateMatrix(&weights[1], middleBias, outputBias, fRand(-1.0, 1.0));
 
     allocateMatrix(&deltaWeightInputMiddle, inputBias, middleBias, 0.0);
     allocateMatrix(&deltaWeightMiddleOutput, middleBias, outputBias, 0.0);
@@ -89,11 +85,13 @@ void initialize(int configuration[], int example, int bias) {
 
 void shutDown() {
     delete[] trainingSetOrder;
-    //deallocate(&inputLayer);
-    deallocate(&inputMiddleWeights);
-    //deallocate(&middleLayer);
-    deallocate(&middleOutputWeights);
-    //deallocate(&outputLayer);
+    deallocate(&layer[0]);
+    deallocate(&weights[0]);
+    deallocate(&layer[1]);
+    deallocate(&weights[1]);
+    deallocate(&layer[2]);
+    delete[] layer;
+    delete[] weights;
     delete[] outputLayerDelta.elements;
     delete[] middleLayerDelta.elements;
     deallocate(&deltaWeightMiddleOutput);
@@ -143,16 +141,16 @@ void randomizeInput() {
 
 void forward(int p) {
     for (auto j = biasSize; j < middleSize + biasSize; j++) {
-        NetworkType activate = inputMiddleWeights.elements[0][j];
+        NetworkType activate = weights[0].elements[0][j];
         for (auto i = biasSize; i < inputSize + biasSize; i++) {
-            activate += layer[0].elements[p + 1][i] * inputMiddleWeights.elements[i][j];
+            activate += layer[0].elements[p + 1][i] * weights[0].elements[i][j];
         }
         layer[1].elements[p][j] = sigmoid(activate);
     }
     for (auto k = biasSize; k < outputSize + biasSize; k++) {
-        NetworkType activate = middleOutputWeights.elements[0][k];
+        NetworkType activate = weights[1].elements[0][k];
         for (auto j = biasSize; j < middleSize + biasSize; j++) {
-            activate += layer[1].elements[p][j] * middleOutputWeights.elements[j][k];
+            activate += layer[1].elements[p][j] * weights[1].elements[j][k];
         }
         layer[2].elements[p][k] = sigmoid(activate);
     }
@@ -167,7 +165,7 @@ void computeError(int p) {
     for (auto j = biasSize; j < middleSize + biasSize; j++) {
         NetworkType activate = 0.0;
         for (auto k = 1; k <= outputSize; k++) {
-            activate += middleOutputWeights.elements[j][k] * outputLayerDelta.elements[k];
+            activate += weights[1].elements[j][k] * outputLayerDelta.elements[k];
         }
         middleLayerDelta.elements[j] = activate * sigmoidDerivative(layer[1].elements[p][j]);
     }
@@ -176,18 +174,18 @@ void computeError(int p) {
 void backpropagate(int p) {
     for (auto k = biasSize; k < outputSize + biasSize; k++) {
         deltaWeightMiddleOutput.elements[0][k] = eta * outputLayerDelta.elements[k] + alpha * deltaWeightMiddleOutput.elements[0][k];
-        middleOutputWeights.elements[0][k] += deltaWeightMiddleOutput.elements[0][k];
+        weights[1].elements[0][k] += deltaWeightMiddleOutput.elements[0][k];
         for (auto j = biasSize; j < middleSize + biasSize; j++) {
             deltaWeightMiddleOutput.elements[j][k] = eta * layer[1].elements[p][j] * outputLayerDelta.elements[k] + alpha * deltaWeightMiddleOutput.elements[j][k];
-            middleOutputWeights.elements[j][k] += deltaWeightMiddleOutput.elements[j][k];
+            weights[1].elements[j][k] += deltaWeightMiddleOutput.elements[j][k];
         }
     }
     for (auto j = biasSize; j < middleSize + biasSize; j++) {
         deltaWeightInputMiddle.elements[0][j] = eta * middleLayerDelta.elements[j] + alpha * deltaWeightInputMiddle.elements[0][j];
-        inputMiddleWeights.elements[0][j] += deltaWeightInputMiddle.elements[0][j];
+        weights[0].elements[0][j] += deltaWeightInputMiddle.elements[0][j];
         for (auto i = biasSize; i < inputSize + biasSize; i++) {
             deltaWeightInputMiddle.elements[i][j] = eta * layer[0].elements[p + 1][i] * middleLayerDelta.elements[j] + alpha * deltaWeightInputMiddle.elements[i][j];
-            inputMiddleWeights.elements[i][j] += deltaWeightInputMiddle.elements[i][j];
+            weights[0].elements[i][j] += deltaWeightInputMiddle.elements[i][j];
         }
     }
 }
